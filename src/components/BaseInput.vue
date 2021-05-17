4ca430e3c81092f386bc66de4abed7932d21d77e
bradley <baeklin.caesar@gmail.com>
2021-05-17 13:30:47 +0900

    keyboard test

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -3,6 +3,8 @@
     <input :inputmode="inputMode"
            ref="input"
            @touchstart="onTouch"
+           @focusout="onFocusOut"
+           @focusin="onFocusIn"
            @keyup.enter="onEnter">
   </div>
 </template>
@@ -17,14 +19,23 @@ export default {
     return {
       inputClicked: false,
       inputMode: 'none',
+      isNotFirstTouch: false,
     };
   },
   methods: {
     onEnter() {
-      this.inputMode = 'none';
       this.$emit('entered');
     },
+    onFocusOut() {
+      this.inputMode = 'none';
+    },
+    onFocusIn() {
+      if (this.isNotFirstTouch) {
+        this.inputMode = 'search';
+      }
+    },
     onTouch() {
+      this.isNotFirstTouch = true;
       if (this.inputMode === 'none') {
         this.inputMode = 'search';
       }
